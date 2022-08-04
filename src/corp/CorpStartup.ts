5d776b5eb7457efd324a3e5c34a18b91510e3785
RyanHx <hollick5@hotmail.com>
2022-08-04 03:28:37 +0100

    Added corp startup script

---
+++ src/corp/CorpStartup.ts
@@ -0,0 +1,100 @@
+import { Corporation, Employee, NS as ns } from '@ns';
+import { calcMaterials } from '/corp/production';
+
+
+const starter_div_name = "agri-0";
+const all_cities: string[] = ["Aevum", "Chongqing", "Sector-12", "New Tokyo", "Ishima", "Volhaven"];
+const positions: string[] = ["Operations", "Engineer", "Business", "Management", "Research & Development"];
+const core_levels = ["FocusWires", "Neural Accelerators", "Speech Processor Implants", "Nuoptimal Nootropic Injector Implants", "Smart Factories", "Smart Storage"];
+let c: Corporation;
+export async function startNewCorp(ns: NS): Promise<void> {
+    c = ns.corporation;
+    c.expandIndustry("Agriculture", starter_div_name);
+    c.unlockUpgrade("Smart Supply");
+    c.setSmartSupply(starter_div_name, "Sector-12", true);
+    await setupCities();
+    for (const upgrade of core_levels.slice(0, 5)) {
+        c.levelUpgrade(upgrade);
+        c.levelUpgrade(upgrade);
+    }
+    await buyMaterials(ns);
+    await getInvestments(ns);
+    for (const city of all_cities) c.upgradeWarehouse(starter_div_name, city, 9);
+    await buyMaterials(ns);
+    c.expandIndustry("Tobacco", "tobacco-0");
+}
+
+async function setupCities() {
+    for (const city of all_cities) {
+        if (city !== "Sector-12") c.expandCity(starter_div_name, city);
+        if (!c.hasWarehouse(starter_div_name, city)) c.purchaseWarehouse(starter_div_name, city);
+        c.setSmartSupply(starter_div_name, city, true);
+        for (let i = 0; i < 3; i++) {
+            const emp = c.hireEmployee(starter_div_name, city);
+            await c.assignJob(starter_div_name, city, emp?.name, positions[i]);
+        }
+        c.upgradeWarehouse(starter_div_name, city, 2);
+        c.sellMaterial(starter_div_name, city, "Food", "MAX", "MP");
+        c.sellMaterial(starter_div_name, city, "Plants", "MAX", "MP");
+    }
+    c.hireAdVert(starter_div_name);
+}
+
+async function buyMaterials(ns: NS): Promise<void> {
+    while (c.getCorporation().state !== "EXPORT") await ns.sleep(1);
+    const division = c.getDivision(starter_div_name);
+    const names = ["Hardware", "Robots", "AI Cores", "Real Estate"];
+    for (const city of division.cities) {
+        if (!c.hasWarehouse(division.name, city)) continue;
+        const warehouse = c.getWarehouse(division.name, city);
+        const mat_counts = calcMaterials(division.type, warehouse.size * 0.6);
+        if (!mat_counts) continue;
+        for (const material of names) {
+            const city_mat_stock = c.getMaterial(division.name, city, material);
+            const req_amount = mat_counts[material] - city_mat_stock.qty
+            if (req_amount > 0) {
+                c.buyMaterial(division.name, city, material, req_amount / 10);
+            } else {
+                c.buyMaterial(division.name, city, material, 0);
+            }
+        }
+    }
+    while (c.getCorporation().state === "EXPORT") await ns.sleep(1);
+    while (c.getCorporation().state !== "EXPORT") await ns.sleep(1);
+    for (const city of division.cities) {
+        if (!c.hasWarehouse(division.name, city)) continue;
+        for (const material of names) {
+            c.buyMaterial(division.name, city, material, 0);
+        }
+    }
+}
+
+async function assignNextEmployees() {
+    for (const city of all_cities) {
+        c.upgradeOfficeSize(starter_div_name, city, 3);
+        c.upgradeOfficeSize(starter_div_name, city, 3);
+        const new_emps: Employee[] = [];
+        for (let i = 0; i < 6; i++) {
+            new_emps.push(c.hireEmployee(starter_div_name, city));
+        }
+        await c.assignJob(starter_div_name, city, new_emps.pop().name, positions[0]);
+        await c.assignJob(starter_div_name, city, new_emps.pop().name, positions[1]);
+        for (let i = 0; i < 2; i++) {
+            await c.assignJob(starter_div_name, city, new_emps.pop().name, positions[3]);
+            await c.assignJob(starter_div_name, city, new_emps.pop().name, positions[4]);
+        }
+        c.upgradeWarehouse(starter_div_name, city, 2);
+    }
+}
+
+async function getInvestments(ns: NS) {
+    while (c.getInvestmentOffer().funds < 210e9) await ns.sleep(1000);
+    c.acceptInvestmentOffer();
+    await assignNextEmployees();
+    while (c.getUpgradeLevel("Smart Factories") < 10) c.levelUpgrade("Smart Factories");
+    while (c.getUpgradeLevel("Smart Storage") < 10) c.levelUpgrade("Smart Storage");
+    for (const city of all_cities) c.upgradeWarehouse(starter_div_name, city, 7);
+    await buyMaterials(ns);
+    while (c.getInvestmentOffer().funds < 5e12) await ns.sleep(1000);
+    c.acceptInvestmentOffer();
+}
--- src/corp/ceo.ts
+++ src/corp/ceo.ts
@@ -1,16 +1,26 @@
 import { NS } from '@ns'
+import { startNewCorp } from '/corp/CorpStartup';
 import { Manager } from '/corp/manager'
 import { OfficeManager } from '/corp/OfficeManager';
 import { WarehouseManager } from '/corp/WarehouseManager';
 
 export async function main(ns: NS): Promise<void> {
+    ns.disableLog("sleep");
+    if (ns.corporation.getCorporation().divisions.length === 0) await startNewCorp(ns);
     const managers: Manager[] = [];
     for (const division of ns.corporation.getCorporation().divisions) {
         managers.push(new OfficeManager(ns, division.name));
         managers.push(new WarehouseManager(ns, division.name));
     }
     while (true) {
+        tryBuyUpgrades(ns);
         if (ns.corporation.getCorporation().state === 'EXPORT') {
+            for (const division of ns.corporation.getCorporation().divisions) {
+                if (!(managers.some(manager => manager.division == division.name))) {
+                    managers.push(new OfficeManager(ns, division.name));
+                    managers.push(new WarehouseManager(ns, division.name));
+                }
+            }
             for (const manager of managers) {
                 await manager.process(ns);
             }
@@ -18,6 +28,28 @@ export async function main(ns: NS): Promise<void> {
                 await ns.sleep(1);
             }
         }
+        if (!ns.corporation.getCorporation().public) {
+            const investment = ns.corporation.getInvestmentOffer();
+            if (investment.round === 3 && investment.funds >= 800e12) {
+                commitFraud(ns, 800e12);
+            } else if (investment.round === 4 && investment.funds >= 1.5e15) {
+                commitFraud(ns, 1.5e15);
+                //ns.corporation.goPublic(0);
+            }
+        }
         await ns.sleep(1);
     }
+}
+
+function tryBuyUpgrades(ns: NS) {
+    const core_levels = ["FocusWires", "Neural Accelerators", "Speech Processor Implants", "Nuoptimal Nootropic Injector Implants", "Smart Factories", "Smart Storage"];
+    const other_levels = ["DreamSense", "Wilson Analytics", "ABC SalesBots", "Project Insight"];
+    const core_upgraded = core_levels.every(upgrade => ns.corporation.getUpgradeLevel(upgrade) >= 20);
+    for (const upgrade of !core_upgraded ? core_levels : core_levels.concat(other_levels)) {
+        if (ns.corporation.getUpgradeLevelCost(upgrade) < ns.corporation.getCorporation().funds * 0.001) ns.corporation.levelUpgrade(upgrade);
+    }
+}
+
+function commitFraud(ns: NS, min_investment: number) {
+    //
 }
