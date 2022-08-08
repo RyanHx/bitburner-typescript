180cadad5b2c34dc5b68d569d88528f7589e2015
RyanHx <hollick5@hotmail.com>
2022-08-09 00:15:50 +0100

    Formulas to get target timings

--- src/utils/targets.ts
+++ src/utils/targets.ts
@@ -4,7 +4,7 @@ export async function main(ns: NS): Promise<void> {
     while (ns.isRunning(root)) {
         await ns.sleep(200);
     }
-
+    const max_weak_time = <number>ns.args[0] ?? 30;
     const target_col_pad = 20;
     const hack_col_pad = 15;
     const mon_col_pad = 13;
@@ -12,9 +12,12 @@ export async function main(ns: NS): Promise<void> {
     servers.sort((a, b) => ns.getServerMaxMoney(b) - ns.getServerMaxMoney(a));
     ns.tprint("Target".padEnd(target_col_pad), "Hack chance".padEnd(hack_col_pad), "Max money");
     for (const target of servers) {
-        const noMoney = ns.getServerMaxMoney(target) <= 0;
-        const unHackable = ns.getServerRequiredHackingLevel(target) > ns.getHackingLevel() * 0.3;
-        if (noMoney || unHackable) continue
+        const serv_obj = ns.getServer(target);
+        serv_obj.hackDifficulty = serv_obj.minDifficulty;
+        serv_obj.moneyAvailable = serv_obj.moneyMax;
+        const noMoney = serv_obj.moneyAvailable <= 0;
+        const unHackable = ns.getServerRequiredHackingLevel(target) > ns.getHackingLevel() * 0.3 || ns.formulas.hacking.weakenTime(serv_obj, ns.getPlayer()) > max_weak_time * 1000;
+        if (noMoney || unHackable) continue;
         ns.tprint(target.padEnd(target_col_pad), ns.hackAnalyzeChance(target).toFixed(3).padEnd(hack_col_pad), ns.getServerMaxMoney(target).toLocaleString('en-GB').padStart(mon_col_pad));
     }
 }
