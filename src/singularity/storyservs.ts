bec3d1d428634c638e0ec8311798b03ed82b6b65
RyanHx <hollick5@hotmail.com>
2022-08-18 02:44:14 +0100

    Merge servs+factions singularity scripts

--- src/singularity/storyservs.ts
+++ src/singularity/storyservs.ts
@@ -2,20 +2,31 @@ import { NS } from '@ns'
 import { getServerConnRoute } from '/utils/locate';
 
 export async function main(ns: NS): Promise<void> {
-    const data = ns.flags([['f', false]]);
+    const data = ns.flags([['F', false]]);
     const root = ns.run("/utils/root.js");
     while (ns.isRunning(root)) await ns.sleep(50);
     const servs = ["CSEC", "avmnite-02h", "I.I.I.I", "run4theh111z"];
     for (const serv of servs) {
+        const target = ns.getServer(serv);
+        if (target.backdoorInstalled || target.requiredHackingSkill > ns.getHackingLevel()) continue;
         for (const step of getServerConnRoute(ns, serv)) {
             ns.singularity.connect(step);
         }
+        ns.tprint(`Installing backdoor on ${serv}.`);
         try {
-            ns.tprint(`Installing backdoor on ${serv}.`);
             await ns.singularity.installBackdoor();
         } catch { ns.tprint("Failed.") }
     }
     ns.singularity.connect("home");
-    ns.tprint("Story servs backdoor attempts finished.");
-    if (<boolean>data.f === true) ns.spawn("/singularity/storyfactions.js");
+    if (<boolean>data.F === true) return;
+    const faction_servs: Record<string, string> = { "CyberSec": "CSEC", "NiteSec": "avmnite-02h", "The Black Hand": "I.I.I.I", "BitRunners": "run4theh111z" };
+    const not_joined_facts = Object.keys(faction_servs).filter(faction => !ns.getPlayer().factions.includes(faction) && ns.getServer(faction_servs[faction]).backdoorInstalled);
+    if (not_joined_facts.length === 0) return;
+    ns.tprint("Waiting for faction invites.");
+    while (not_joined_facts.some(faction => !ns.getPlayer().factions.includes(faction))) {
+        await ns.sleep(100);
+        for (const faction of not_joined_facts) {
+            ns.singularity.joinFaction(faction);
+        }
+    }
 }
