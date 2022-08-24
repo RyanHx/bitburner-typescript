921396c0c5659ecafdbbc63144059085816c7fcb
RyanHx <hollick5@hotmail.com>
2022-08-24 20:48:00 +0100

    Scale from current hashes

--- src/hashnet/upgrades.ts
+++ src/hashnet/upgrades.ts
@@ -13,30 +13,27 @@ export async function main(ns: NS): Promise<void> {
         ['bsp', false], // BB skill points
         ['br', false] // BB ranks
     ]);
+    const budget = () => ns.hacknet.numHashes() * 0.25;
     while (true) {
         const targetMinSec = ns.getServer(<string>data['target']).minDifficulty;
-        const maxHash = ns.hacknet.hashCapacity();
-        if (ns.hacknet.numHashes() > maxHash * 0.9) {
-            const budget = Math.floor(maxHash * 0.25);
-            if (budget > ns.hacknet.hashCost("Reduce Minimum Security") && targetMinSec > 1 && data['sec']) {
-                ns.hacknet.spendHashes("Reduce Minimum Security", <string>data['target']);
-            } else if (budget > ns.hacknet.hashCost("Increase Maximum Money") && data['mon']) {
-                ns.hacknet.spendHashes("Increase Maximum Money", <string>data['target']);
-            } else if (budget > ns.hacknet.hashCost("Improve Studying") && data['study']) {
-                ns.hacknet.spendHashes("Improve Studying");
-            } else if (budget > ns.hacknet.hashCost("Improve Gym Training") && data['gym']) {
-                ns.hacknet.spendHashes("Improve Gym Training");
-            } else if (budget > ns.hacknet.hashCost("Sell for Corporation Funds") && ns.getPlayer().hasCorporation && data['fund']) {
-                ns.hacknet.spendHashes("Sell for Corporation Funds");
-            } else if (budget > ns.hacknet.hashCost("Exchange for Corporation Research") && ns.getPlayer().hasCorporation && data['res']) {
-                ns.hacknet.spendHashes("Exchange for Corporation Research");
-            } else if (budget > ns.hacknet.hashCost("Exchange for Bladeburner SP") && ns.getPlayer().inBladeburner && data['bsp']) {
-                ns.hacknet.spendHashes("Exchange for Bladeburner SP");
-            } else if (budget > ns.hacknet.hashCost("Exchange for Bladeburner Rank") && ns.getPlayer().inBladeburner && data['br']) {
-                ns.hacknet.spendHashes("Exchange for Bladeburner Rank");
-            } else {
-                ns.hacknet.spendHashes("Sell for Money");
-            }
+        if (budget() > ns.hacknet.hashCost("Reduce Minimum Security") && targetMinSec > 1 && data['sec']) {
+            ns.hacknet.spendHashes("Reduce Minimum Security", <string>data['target']);
+        } else if (budget() > ns.hacknet.hashCost("Increase Maximum Money") && data['mon']) {
+            ns.hacknet.spendHashes("Increase Maximum Money", <string>data['target']);
+        } else if (budget() > ns.hacknet.hashCost("Improve Studying") && data['study']) {
+            ns.hacknet.spendHashes("Improve Studying");
+        } else if (budget() > ns.hacknet.hashCost("Improve Gym Training") && data['gym']) {
+            ns.hacknet.spendHashes("Improve Gym Training");
+        } else if (budget() > ns.hacknet.hashCost("Sell for Corporation Funds") && ns.getPlayer().hasCorporation && data['fund']) {
+            ns.hacknet.spendHashes("Sell for Corporation Funds");
+        } else if (budget() > ns.hacknet.hashCost("Exchange for Corporation Research") && ns.getPlayer().hasCorporation && data['res']) {
+            ns.hacknet.spendHashes("Exchange for Corporation Research");
+        } else if (budget() > ns.hacknet.hashCost("Exchange for Bladeburner SP") && ns.getPlayer().inBladeburner && data['bsp']) {
+            ns.hacknet.spendHashes("Exchange for Bladeburner SP");
+        } else if (budget() > ns.hacknet.hashCost("Exchange for Bladeburner Rank") && ns.getPlayer().inBladeburner && data['br']) {
+            ns.hacknet.spendHashes("Exchange for Bladeburner Rank");
+        } else if (ns.hacknet.numHashes() > ns.hacknet.hashCapacity() * 0.9) {
+            ns.hacknet.spendHashes("Sell for Money");
         }
         await ns.sleep(100);
     }
